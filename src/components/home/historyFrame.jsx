1debd7c425bd4f3ed6743dc5ccc8c788a4b77508
Yeongi Park <yeongi0111@naver.com>
2024-07-16 15:51:28 +0900

    홈 오른쪽 historyframe 디자인 완료

--- src/components/home/historyFrame.jsx
+++ src/components/home/historyFrame.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { IoIosCloseCircle } from "react-icons/io";
 
-export const historyFrame = () => {
-  return <div>historyFrame</div>;
+export const HistoryFrame = () => {
+  return (
+    <div className="flex gap-5 relative mb-4">
+      <div className="bg-gray-300 w-14 h-14"></div>
+      <div>
+        <p className="mb-2">fileName.pdf</p>
+        <p className="text-sm text-gray-400">안녕하세요</p>
+      </div>
+      <div className="absolute right-3 top-3 hover:bg-slate-400 rounded-3xl cursor-pointer">
+        <IoIosCloseCircle size={30} />
+      </div>
+    </div>
+  );
 };
--- src/components/home/right.jsx
+++ src/components/home/right.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { HistoryFrame } from "./historyFrame";
 
 export const Right = () => {
   return (
     <div className="w-1/3 rounded-md bg-neutral-100 p-5 h-72">
       <h2 className="font-bold text-xl mb-2">Recent Translations</h2>
-      {localStorage.getItem("login") ? (
-        <>아이템</>
+      {!localStorage.getItem("login") ? (
+        <>
+          <HistoryFrame />
+          <HistoryFrame />
+          <HistoryFrame />
+        </>
       ) : (
         <p className="text-gray-500 text-sm">
           If you want to see your records, please login
